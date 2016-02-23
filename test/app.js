1e4cbb9b3db37f88fca01a5b61094dff0dc7f01b
Lucas Aragno <lucas.aragno@moove-it.com>
2016-02-23 11:21:55 -0300

    Add important files
    Change-Id: I28c23f20500f8cacd1e8cc35ed91be9330b59dfa

--- test/app.js
+++ test/app.js
@@ -6,14 +6,15 @@ var helpers = require('yeoman-generator').test
 describe('generator-remoox:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({someOption: true})
-      .withPrompts({someAnswer: true})
       .on('end', done)
   })
 
   it('creates files', function () {
     assert.file([
-      'server.js'
+      'server.js',
+      'package.js',
+      'main.css',
+      'webpack.config.js'
     ])
   })
 })
